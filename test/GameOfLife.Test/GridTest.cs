9e38eeee707c8f7803880aebd1e2c88a37956883
Eric Tuvesson <eric.tuvesson@gmail.com>
2015-05-02 19:08:47 +0200

    Added missing files.

---
+++ test/GameOfLife.Test/GridTest.cs
@@ -0,0 +1,37 @@
+﻿namespace GameOfLife
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Xunit;
+    
+    public class GridTest
+    {
+        const int size = 100;
+
+        private Grid<bool> grid;
+
+        public GridTest()
+        {
+            var rd = new Random(20150501);
+
+            this.grid = new Grid<bool>(size, size);
+
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    if (rd.Next(0, 10) > 5)
+                        this.grid.SetCell(x, y, true);
+        }
+
+        [Fact]
+        public void to_buffer()
+        {
+            var list = new List<UnlimitedSimulation.Point>();
+
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    if (grid.GetCell(x, y))
+                        list.Add(new UnlimitedSimulation.Point(x, y));
+        }
+    }
+}
---
+++ test/GameOfLife.Windows/MultiCellEffect.cs
@@ -0,0 +1,52 @@
+﻿namespace GameOfLife
+{
+	using SharpDX;
+	using SharpDX.D3DCompiler;
+	using SharpDX.Direct3D11;
+	using SharpDX.DXGI;
+
+	public struct RectVertex
+	{
+		public Vector4 pos;
+		public Vector2 siz; // TODO: I could remove the size 
+		public Vector4 col;
+	}
+
+	public sealed class MultiCellEffect
+	{
+		/// <summary> </summary>
+		public InputLayout Layout { get; private set; }
+
+		/// <summary> </summary>
+		public VertexShader VertexShader { get; private set; }
+
+		/// <summary> </summary>
+		public GeometryShader GeometryShader { get; private set; }
+
+		/// <summary> </summary>
+		public PixelShader PixelShader { get; private set; }
+
+		public MultiCellEffect(SharpDX.Direct3D11.Device device)
+		{
+			// Compile Vertex Shader
+			var bytecode = CustomEffect.VertexShaderByteCode;
+
+			this.VertexShader = new VertexShader(device, bytecode);
+			this.Layout = new InputLayout(device, ShaderSignature.GetInputSignature(bytecode), new[] 
+			{ 
+				new InputElement("ANCHOR", 0, Format.R32G32B32A32_Float, 0, 0), 
+				new InputElement("DIMENSIONS", 0, Format.R32G32_Float, 16, 0), 
+				new InputElement("COLOR", 0, Format.R32G32B32A32_Float, 24, 0) 
+			});
+
+			// Compile Geometry Shader
+			bytecode = CustomEffect.GeometryShaderByteCode;
+			this.GeometryShader = new GeometryShader(device, bytecode);
+
+			// Compile Pixel Shader
+			bytecode = CustomEffect.PixelShaderByteCode;
+			this.PixelShader = new PixelShader(device, bytecode);
+		}
+	}
+
+}
